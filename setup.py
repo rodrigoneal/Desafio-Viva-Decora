855773e847f1ea63e2656d0c885abca8df33d870
rodrigoneal <rodrigho2006@gmail.com>
2021-02-01 17:52:49 -0300

    Criando um arquivo instalavel

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     description="List all files in a github repository and save in txt",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["httpx", "beautifulsoup4"]
-
+    install_requires=["httpx", "beautifulsoup4"],
 )
