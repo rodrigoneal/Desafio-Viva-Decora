93233df08b041ee5405c8d3e8e44db204770f65c
rodrigoneal <rodrigho2006@gmail.com>
2021-02-02 18:31:45 -0300

    removendo comentario por incompatibilidade com os linters

--- github/clear_code.py
+++ github/clear_code.py
@@ -1,8 +1,4 @@
-"""Esse script é usado para refinar os dados do aplicativo.
-
-Author: Rodrigo Castro
-Date: 1/02/2021
-"""
+"""Esse script é usado para refinar os dados do aplicativo."""
 from typing import Dict, Union, Optional
 
 
